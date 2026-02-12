7e3549fc2c73aa2093f21f7941733ac75ec683e1
Lhyca Mae <lhycamae2002@outlook.com>
2026-02-12 19:40:56 +0900

    Register models in Django admin

--- rides/admin.py
+++ rides/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User, Ride, RideEvent
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id_user', 'username', 'email', 'role')
+
+
+@admin.register(Ride)
+class RideAdmin(admin.ModelAdmin):
+    list_display = ('id_ride', 'status', 'rider', 'driver', 'pickup_time')
+    list_filter = ('status',)
+
+
+@admin.register(RideEvent)
+class RideEventAdmin(admin.ModelAdmin):
+    list_display = ('id_ride_event', 'ride', 'description', 'created_at')
